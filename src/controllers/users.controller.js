b518bdf0a26542e42ebbd1955caf9bf2ae006c33
Jeseungyou <jeseungyou@pop-os.localdomain>
2022-12-27 16:52:55 -0500

    FIX: test log removed - 2

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -35,17 +35,13 @@ class UsersController {
     loginUser = async (req, res, next) => {
         try {
             const { email, password } = req.body;
-            console.log(email)
 
             if (!email || !password) {
-                console.log('yes')
                 throw new InvalidParamsError();
             }
 
             // call loginUser in service - log in with id and pw
             const {accessToken, refreshToken} = await this.UsersService.loginUser(email, password);
-            console.log(accessToken)
-            console.log(refreshToken)
 
             // return authentication: token -> to pass the token to client
             return res.status(200).json({
